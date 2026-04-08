fd6bce20d8091c43069a0494571218f24225819f
Camila Murcia <mariamurmu@unisabana.edu.co>
2026-04-07 22:13:47 -0500

    e4

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,14 @@
 # Write your code here!
+def employee_print(employee_info):
+    print("Name:", employee_info.get("Name", "N/A"))
+    print("Salary:", employee_info.get("Salary", "N/A"))
+    print("Role:", employee_info.get("Role", "N/A"))
+
+    employee_info.pop("Name", None)
+    employee_info.pop("Salary", None)
+    employee_info.pop("Role", None)
+    if len(employee_info) == 0:
+        print("No other info!")
+    else:
+        for key, value in employee_info.items():
+            print(key , value)
