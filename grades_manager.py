f9d89ecf2cf3c8684a622dd9460274060aa25514
Camila Murcia <mariamurmu@unisabana.edu.co>
2026-04-07 23:09:10 -0500

    p2

--- grades_manager.py
+++ grades_manager.py
@@ -2,3 +2,26 @@
 def initialize_dict(student_name, subject_grades):
     result = {student_name: subject_grades}
     return result
+
+#P2
+def add_student(student_grades= None):
+    if student_grades == None:
+        student_grades = {}
+    Name = input("Enter student name: ")
+    Name = Name.title()
+
+    subjects = {}
+    while True:
+        Materias_Notas = input("Enter subject and grade (or 'exit' to finish): ")
+
+        if Materias_Notas.lower() == "exit":
+            break
+        Subject, grade = Materias_Notas.split(",")
+        Subject = Subject.title()
+        grade = float(grade)
+
+        subjects[Subject]= grade
+    student_grades[Name]= subjects
+
+    print(f"Student {Name} successfully added to the grades management system.")
+    return student_grades
