cc0e718dd54bced2e151895c7a594f33d5d5557b
Camila Murcia <mariamurmu@unisabana.edu.co>
2026-04-07 21:57:03 -0500

    ej 1

--- dict_concept.py
+++ dict_concept.py
@@ -1,1 +1,9 @@
 # Write your code here!
+def get_config(): 
+    datos = {
+        "width": 480, 
+        "height": 480, 
+        "color_mode": "dark", 
+        "sensitivity": float(0.4)
+        }
+    return datos 
